b58a00dc9f5cad08def327c369e8d25a6eddd369
pudinfg <javierticonax3@gmail.com>
2024-07-22 18:40:43 -0500

    Create pudinfg.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/pudin/pudinfg.py
@@ -0,0 +1,23 @@
+#https://www.python.org/
+
+#comentario en una linea
+
+¨¨¨
+Esto tambien es 
+un comentario 
+en varias lineas
+¨¨¨
+
+my_variable = ¨Mi variable¨
+my_variable = ¨Nuevo valor de mi variable¨
+
+MY_CONSTANT = ¨Mi constate¨ #por convencion
+
+my_int = 1
+my_float = 0.5
+my_bool = True
+my_bool = False
+my_string = ¨mi candena de texto¨
+my_other_string = ¨mi otra cadena de texto¨
+
+print("¡Hola, Python!")
